5d20a0da0bb8efa524d24685d1a4df142b75835d
Maria Constanza Alegre <alegremaria_cen@ucp.edu.ar>
2023-06-08 17:20:47 -0300

    Primer commit

---
+++ app.js
@@ -0,0 +1,69 @@
+let nombre;
+let edad;
+let preferencias;
+const blanco = " ";
+const ficcion = 1;
+const accion = 2;
+const comedia = 3;
+const romantica = 4;
+let continuar;
+let titulo;
+let cantidad;
+let precio = 550;
+let costoTotal;
+
+nombre = prompt("¿Cómo es tu nombre?");
+console.log(nombre);
+if (nombre != "") {
+    edad = prompt("Hola" + blanco + nombre + blanco + "¿Cuántos años tenes?");
+    console.log(edad);
+    if (edad < 16) {
+        console.log("Mostrar peliculas infantiles");
+        if (edad < 12 && edad > 15) {
+            console.log("No mostrar nada mas");
+        } else {
+            console.log("Mostrar tambien peliculas para adolescentes")
+        }
+    } else {
+        console.log("Mostrar peliculas +16");
+        do {
+            console.log("Eligiendo categorias");
+            preferencias = parseInt(
+                prompt("¿Qué tipo de películas preferis? 1- Ficcion 2- Accion 3- Comedia 4- Romantica"));
+            switch (preferencias) {
+                case ficcion:
+                    console.log("Mostrar peliculas de ficcion");
+                    break;
+                case accion:
+                    console.log("Mostrar peliculas de accion");
+                    break;
+                case comedia:
+                    console.log("Mostrar peliculas de comedia");
+                    break;
+                case romantica:
+                    console.log("Mostrar peliculas romanticas");
+                    break;
+            }
+            continuar = prompt("Desea elegir otra otra categoria? si/no")
+
+        } while (continuar == si);
+    }
+    if (continuar != si) {
+        titulo = prompt("Ingrese el titulo de la pelicula del cual quiera adquirir tickets");
+    } if (titulo !== "") {
+        cantidad = prompt("Ingrese la cantidad de entradas que desea comprar");
+    } if (cantidad !== "") {
+        alert("El costo a abonar es de: $" + precio * cantidad)
+    }
+}
+
+
+
+if (nombre == "" || edad == "" || preferencias == "" || titulo == "" || cantidad == "") {
+    alert("Por favor, completar los datos solicitados");
+}
+
+
+
+
+
